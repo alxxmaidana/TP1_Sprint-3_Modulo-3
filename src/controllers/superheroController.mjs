6bc22b98d260e2edfed40cd383826a0a32b6c2df
Alexander Maidana <maidanaalexanderemanuel@gmail.com>
2026-04-06 15:35:19 -0300

    Corección salida de errores en el controlador

--- src/controllers/superheroController.mjs
+++ src/controllers/superheroController.mjs
@@ -21,15 +21,15 @@ export async function obtenerSuperheroePorIdController(req, res) {
 		const { id } = req.params;
 		const superheroe = await obtenerSuperheroePorId(id);
 		if (superheroe === null) {
-			res.status(404).send({ mensaje: "Superhéroe no encontrado" });
+			return res.status(404).send({ mesagge: "Superhéroe no encontrado" });
 		} else {
 			const superheroeFormateado = renderizarSuperheroe(superheroe);
 			res.status(200).json(superheroeFormateado);
 		}
 	} catch (err) {
 		res.status(500).send({
-			mensaje: "Error al buscar el superhéroe",
-			err: err.mensaje,
+			mesagge: "Error al buscar el superhéroe",
+			err: err.mesagge,
 		});
 	}
 }
@@ -42,8 +42,8 @@ export async function obtenerTodosLosSuperheroesController(_req, res) {
 		res.status(200).json(superheroesFormateados);
 	} catch (err) {
 		res.status(500).send({
-			mensaje: "Error al obtener todos los superhéroes",
-			err: err.mensaje,
+			mesagge: "Error al obtener todos los superhéroes",
+			err: err.mesagge,
 		});
 	}
 }
@@ -54,8 +54,8 @@ export async function buscarSuperheoresPorAtributoController(req, res) {
 		const { atributo, valor } = req.params;
 		const superheroes = await buscarSuperheoresPorAtributo(atributo, valor);
 		if (superheroes.length === 0) {
-			res.status(404).send({
-				mensaje: "No se encontraros superhéroes con ese atributo",
+			return res.status(404).send({
+				mesagge: "No se encontraros superhéroes con ese atributo",
 			});
 		} else {
 			const superheroesFormateados = renderizarlistaSuperheroes(superheroes);
@@ -63,8 +63,8 @@ export async function buscarSuperheoresPorAtributoController(req, res) {
 		}
 	} catch (err) {
 		res.status(500).send({
-			mensaje: "Error al buscar sueperhéroes por atributo",
-			err: err.mensaje,
+			mesagge: "Error al buscar sueperhéroes por atributo",
+			err: err.mesagge,
 		});
 	}
 }
@@ -74,8 +74,8 @@ export async function obtenerSuperheroesMayoresDe30Controller(_req, res) {
 	try {
 		const superheroes = await obtenerSuperheroesMayoresDe30();
 		if (superheroes.length === 0) {
-			res.status(404).send({
-				mensaje: "No se encontraron superhéroes mayores a 30 años",
+			return res.status(404).send({
+				mesagge: "No se encontraron superhéroes mayores a 30 años",
 			});
 		} else {
 			const superheroesFormateados = renderizarlistaSuperheroes(superheroes);
@@ -83,8 +83,8 @@ export async function obtenerSuperheroesMayoresDe30Controller(_req, res) {
 		}
 	} catch (err) {
 		res.status(500).send({
-			mensaje: "Error al buscar superhéros mayores a 30 años",
-			err: err.mansaje,
+			mesagge: "Error al buscar superhéros mayores a 30 años",
+			err: err.mesagge,
 		});
 	}
 }
@@ -94,30 +94,30 @@ export async function agregarNuevoSuperheroeController(_req, res) {
 	try {
 		// Crear nuevo superhéroe
 		const nuevoSuperheroe = new Superhero({
-			nombreSuperheroe: "Black Panther",
-			nombreReal: "T'Challa",
-			edad: 35,
-			planetaOrigen: "Tierra",
-			debilidad: "Vibranium anti-metal",
+			nombreSuperheroe: "Silver Surfer",
+			nombreReal: "Norrin Radd",
+			edad: 10000,
+			planetaOrigen: "Zenn-La",
+			debilidad: "Barrera cósmica que lo confina a la Tierra",
 			poderes: [
-				"Traje de vibranium",
-				"Superfuerza",
-				"Superagilidad",
-				"Sentidos mejorados",
-				"Maestro en artes marciales",
+				"Poder Cósmico",
+				"Vuelo interestelar",
+				"Manipulación de energía",
+				"Casi inmortalidad",
+				"Tabla de plata indestructible",
 			],
-			aliados: ["Capitán América", "Iron Man", "Okoye"],
-			enemigos: ["Killmonger", "Ulysses Klaue", "M'Baku"],
-			creador: "Stan Lee",
+			aliados: ["Fantastic Four", "Thor", "Adam Warlock"],
+			enemigos: ["Galactus", "Thanos", "Mephisto"],
+			creador: "Jack Kirby",
 		});
-		// Agregar superhéroe a la db
+		// Agregar superhéroe a la DB
 		await agregarNuevoSuperheroe(nuevoSuperheroe);
 		const nuevoSuperheroeFormateado = renderizarSuperheroe(nuevoSuperheroe);
 		res.status(200).json(nuevoSuperheroeFormateado);
 	} catch (err) {
 		res.status(500).send({
-			mensaje: "Error al agregar el nuevo superheroe",
-			err: err.mensaje,
+			mesagge: "Error al agregar el nuevo superheroe",
+			err: err.mesagge,
 		});
 	}
 }
@@ -129,8 +129,8 @@ export async function eliminarSuperheroePorNombreController(req, res) {
 		const superheroeEliminado =
 			await eliminarSuperheroePorNombre(nombreSuperheroe);
 		if (!superheroeEliminado) {
-			res.status(404).send({
-				mensaje: `No se encotró el superhéroe ${nombreSuperheroe}, no se puede eliminar`,
+			return res.status(404).send({
+				mesagge: `No se encotró el superhéroe ${nombreSuperheroe}, no se puede eliminar`,
 			});
 		} else {
 			const superheroeFormateado = renderizarSuperheroe(superheroeEliminado);
@@ -138,8 +138,8 @@ export async function eliminarSuperheroePorNombreController(req, res) {
 		}
 	} catch (err) {
 		res.status(500).send({
-			mensaje: "Error al eliminar el superheroe",
-			err: err.mensaje,
+			mesagge: "Error al eliminar el superheroe",
+			err: err.mesagge,
 		});
 	}
 }
@@ -152,8 +152,8 @@ export async function eliminarSuperheroePorIdController(req, res) {
 		const superheroeEliminado = await eliminarSuperheroePorId(id);
 		console.log(superheroeEliminado);
 		if (!superheroeEliminado) {
-			res.status(404).send({
-				mensaje: "Nose encontró el superhéroe, no se puede eliminar",
+			return res.status(404).send({
+				mesagge: "Nose encontró el superhéroe, no se puede eliminar",
 			});
 		} else {
 			const superheroeFormateado = renderizarSuperheroe(superheroeEliminado);
@@ -161,8 +161,8 @@ export async function eliminarSuperheroePorIdController(req, res) {
 		}
 	} catch (err) {
 		res.status(500).send({
-			mensaje: "Error al elminar el superhéroe",
-			err: err.mensaje,
+			mesagge: "Error al elminar el superhéroe",
+			err: err.mesagge,
 		});
 	}
 }
@@ -178,8 +178,8 @@ export async function actualizarSuperheroePorIdController(req, res) {
 
 		// Verificar si se encontró el superheroe
 		if (superheroeEncotrado === null) {
-			res.status(404).send({
-				mensaje: "No se encontró el superhéroe, no se pudo actualizar",
+			return res.status(404).send({
+				mesagge: "No se encontró el superhéroe, no se pudo actualizar",
 			});
 		} else {
 			const superheroes = await obtenerTodosLosSuperheroes();
@@ -188,8 +188,8 @@ export async function actualizarSuperheroePorIdController(req, res) {
 		}
 	} catch (err) {
 		res.status(500).send({
-			mensaje: "Error al actualizar el superhéroe",
-			err: err.mensaje,
+			mesagge: "Error al actualizar el superhéroe",
+			err: err.mesagge,
 		});
 	}
 }
